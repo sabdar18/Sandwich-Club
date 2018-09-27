8473f8e044a87a3c3cb53c7b3285d22729e3adc6
SABDAR SHAIK <developer.sabdar@gmail.com>
2018-09-27 08:52:01 +0530

    Project Specifications Implemented

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,18 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView imageView = findViewById(R.id.imageView);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +49,12 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
+        populateUI(sandwich);
+        Picasso.get()
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(imageView);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +64,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView alsoKnownAs = findViewById(R.id.alsoKnown);
+        TextView alsoKnownAsTextView = findViewById(R.id.also_knownTextView);
+        TextView ingredientsTextView = findViewById(R.id.ingredientsTextView);
+        TextView placeOfOriginTextView = findViewById(R.id.placeOfOriginTextView);
+        TextView descriptionTextView = findViewById(R.id.descriptionTextView);
 
+        StringBuilder alsoKnownSB = new StringBuilder();
+        List<String> alsoKnownList = sandwich.getAlsoKnownAs();
+        if(alsoKnownList.size() >0) {
+            for (int i = 0; i < alsoKnownList.size(); i++) {
+                alsoKnownSB.append(alsoKnownList.get(i));
+                if (i < alsoKnownList.size() - 1) {
+                    alsoKnownSB.append(", ");
+                }
+            }
+           alsoKnownAsTextView.setText(alsoKnownSB.toString());
+        }
+
+        StringBuilder ingredientsSB = new StringBuilder();
+        List<String> ingredientsList= sandwich.getIngredients();
+        for(int i= 0; i< ingredientsList.size(); i++){
+            ingredientsSB.append(ingredientsList.get(i));
+            if(i < ingredientsList.size()-1){
+                ingredientsSB.append("\n");
+            }
+        }
+
+        ingredientsTextView.setText(ingredientsSB.toString());
+
+        placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+        descriptionTextView.setText(sandwich.getDescription());
     }
 }
